9e00baaab505c4de0b1eaee278ef8fcf24dc4132
Nikhil Golla <nikhiljosh@Nikhils-Laptop.local>
2024-01-23 23:36:46 -0600

    Initial Commit

---
+++ src/main/java/com/app/openweatherapi/OpenWeatherCustomApiApplication.java
@@ -0,0 +1,13 @@
+package com.app.openweatherapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenWeatherCustomApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OpenWeatherCustomApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/app/openweatherapi/OpenweatherCustomApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.openweatherapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenweatherCustomApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
